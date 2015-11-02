229e4631ea4a093eb2153fdef0f747b848c94d93
Artur Nowak <artur.nowak@evidenceprime.com>
2015-11-02 10:50:35 +0100

    Increased request size limit for Nginx

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'phabricator'
-maintainer       'Jelmer Snoeck'
-maintainer_email 'jelmer.snoeck@gmail.com'
+maintainer       'Artur Nowak'
+maintainer_email 'artur.nowak@evidenceprime.com'
 license          'BSD'
 description      'Chef cookbook to install Phabricator'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
